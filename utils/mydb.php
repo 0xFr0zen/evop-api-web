0b4a598bec1c234c68e242ac9f52836cfe137b75
0xFr0zen <0xfr0zen@gmail.com>
2020-06-27 22:27:23 +0200

    Update mydb.php

--- utils/mydb.php
+++ utils/mydb.php
@@ -1,16 +1,17 @@
 <?php
 include_once '../../essentials/DBConnector.php';
+$myconfig = json_decode(file_get_contents("./config.json"), true)['db']['connectors'];
 class MyAdminDBConnector extends DBConnector {
     
     public function __construct(){
-        $myconfig = json_decode(file_get_contents("./config.json"), true)['db']['connectors']['admin'];
-        parent::__construct($myconfig['username'],$myconfig['password'],$myconfig['database']);
+        $myconfig2 = $myconfig['admin'];
+        parent::__construct($myconfig2['username'],$myconfig2['password'],$myconfig2['database']);
     }
 }
 class MyCompanyDBConnector extends DBConnector {
     
     public function __construct(){
-        $myconfig = json_decode(file_get_contents("./config.json"), true)['db']['connectors']['company'];
-        parent::__construct($myconfig['username'],$myconfig['password'],$myconfig['database']);
+        $myconfig2 = $myconfig['company'];
+        parent::__construct($myconfig2['username'],$myconfig2['password'],$myconfig2['database']);
     }
 }

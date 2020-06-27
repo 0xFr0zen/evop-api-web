f2acc5bd02b0cc5bdf88fdb4acf0de2f758e3224
0xFr0zen <0xfr0zen@gmail.com>
2020-06-27 22:48:02 +0200

    Update admin.js

--- res/admin.js
+++ res/admin.js
@@ -6,20 +6,20 @@ $(document).ready((_) => {
     checkmodrequestsintervall = setInterval(checkmodrequests, 20000);
 });
 function checklogin() {
-    $.getJSON('/evop/api/mod-login-status', (data) => {
+    $.getJSON('https://api.ev-op.de/mod-login-status', (data) => {
         if (!data.result.loggedin) {
             // document.location.href = 'http://fr0zenofficial.bplaced.net/test-apps/';
         }
     });
 }
 function checkmodrequests() {
-    $.getJSON('/evop/api/mod-requests', (data) => {
+    $.getJSON('https://api.ev-op.de/mod-requests', (data) => {
         if (data.result.mod_requests) {
             let arr = data.result.mod_requests;
             $('#moderators').empty();
             arr.forEach((req_mod) => {
                 $.get(
-                    `/evop/api/mod-card/${req_mod.deviceid}/${req_mod.ip}/${req_mod.sessionid}/${req_mod.request_time}/${req_mod.code}`,
+                    `https://api.ev-op.de/mod-card/${req_mod.deviceid}/${req_mod.ip}/${req_mod.sessionid}/${req_mod.request_time}/${req_mod.code}`,
                     (card) => {
                         $('#moderators').append(card);
                     },

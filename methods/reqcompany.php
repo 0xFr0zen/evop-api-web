4ca830c71029631ba8e5944e808448ac7b121450
0xFr0zen <0xfr0zen@gmail.com>
2020-06-28 20:42:50 +0200

    Update reqcompany.php

--- methods/reqcompany.php
+++ methods/reqcompany.php
@@ -13,28 +13,34 @@ class ReqCompany {
         $this->details = $_REQUEST['details'];
         $this->comp = $_REQUEST['company'];
         $reqs = array_keys($_REQUEST);
+        $found = false;
         foreach ($reqs as $key => $value) {
-            switch ($key) {
-                case 'companies':
-                    $dbconn = new MyCompanyDBConnector();
-                    $sql = "SELECT `name`, `tables` FROM company";
-                    $resultCompanies = $dbconn->query($sql);
-                    $resulter = array();
-                    while (($r = $resultCompanies->fetch_assoc()) != null) {
-                        array_push($resulter, $r);
-                    }
-                    $this->result = array("result" => $resulter);
-                    die(json_encode($this->result, JSON_NUMERIC_CHECK));
-                    break;
-                case 'company':
-                    $this->company = new Company($this->comp);
-                    $this->exists = $this->company->exists();
-                    break;
-                break;
-                default:
-                    $this->result = array("error" => Company::$SPECIFY_A_COMPANYNAME);
-                    die(json_encode($this->result, JSON_NUMERIC_CHECK));
-                    break;
+            if(!$found) {
+                switch ($key) {
+                    case 'companies':
+                        $dbconn = new MyCompanyDBConnector();
+                        $sql = "SELECT `name`, `tables` FROM company";
+                        $resultCompanies = $dbconn->query($sql);
+                        $resulter = array();
+                        while (($r = $resultCompanies->fetch_assoc()) != null) {
+                            array_push($resulter, $r);
+                        }
+                        $this->result = array("result" => $resulter);
+                        $found = true;
+                        die(json_encode($this->result, JSON_NUMERIC_CHECK));
+                        break;
+                    case 'company':
+                        $this->company = new Company($this->comp);
+                        $this->exists = $this->company->exists();
+                        $found = true;
+                        break;
+                    
+                    default:
+                        $this->result = array("error" => Company::$SPECIFY_A_COMPANYNAME);
+                        $found = true;
+                        die(json_encode($this->result, JSON_NUMERIC_CHECK));
+                        break;
+                }
             }
         }
         

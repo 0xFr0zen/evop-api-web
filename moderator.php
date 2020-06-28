3b143fc1d34ca4f51ff5dcd7bf2c7f3ea754743c
0xFr0zen <0xfr0zen@gmail.com>
2020-06-28 22:38:57 +0200

    Update moderator.php

--- moderator.php
+++ moderator.php
@@ -1,31 +1,25 @@
 <?php
-
-if (isset($_REQUEST['modrequests'])) {
-    $dbconn = new MyAdminDBConnector();
-    
-    if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_REQUEST['did']) && isset($_REQUEST['sid'])) {
-        $did = $_REQUEST['did'];
-        $sid = $_REQUEST['sid'];
-        $lgd = $dbconn->addModRequest($did, $sid);
-    }
-    $mrqs = $dbconn->getModRequests();
-    $result = array("result" => array("mod_requests" => $mrqs));
-    die(json_encode($result));
-}
-if (isset($_REQUEST['loginstatus'])) {
-    $dbconn = new MyAdminDBConnector();
-    $mrqs = $dbconn->checklogin();
-    if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_REQUEST['did']) && isset($_REQUEST['sid'])) {
-        $did = $_REQUEST['did'];
-        $sid = $_REQUEST['sid'];
-        $mrqs = $dbconn->addModRequest($did, $sid);
-    }else {
-        if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_REQUEST['did']) && isset($_REQUEST['sid'])) {
-            $did = $_REQUEST['did'];
-            $sid = $_REQUEST['sid'];
-            $mrqs = $dbconn->addModRequest($did, $sid);
+include_once __DIR__."/../../essentials/moderator.php";
+$result = array();
+if (isset($_REQUEST['did']) && isset($_REQUEST['sid'])) {
+    $moderator = new Moderator(
+        $_REQUEST['did'],
+        $_REQUEST['sid']
+    );
+    $reqs = array_keys($_REQUEST);
+    foreach($reqs as $key => $value){
+        $r = $_REQUEST[$key];
+        if(!$found){
+            switch ($variable) {
+                case 'modrequests':
+                    $result = $moderator->request();
+                    $found = true;
+                    break;
+                case 'loginstatus':
+                    $result = $moderator->checklogin();
+                break;
+            }
         }
+        
     }
-    $result = array("result" => array("requestparams" => $mrqs));
-    die(json_encode($result));
 }

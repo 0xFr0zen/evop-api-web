89f776c73d25e0b7827001138427b0c4b44cda19
0xFr0zen <0xfr0zen@gmail.com>
2020-06-28 22:00:51 +0200

    updates

--- moderator.php
+++ moderator.php
@@ -12,3 +12,20 @@ if (isset($_REQUEST['modrequests'])) {
     $result = array("result" => array("mod_requests" => $mrqs));
     die(json_encode($result));
 }
+if (isset($_REQUEST['loginstatus'])) {
+    $dbconn = new MyAdminDBConnector();
+    $mrqs = $dbconn->checklogin();
+    if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_REQUEST['did']) && isset($_REQUEST['sid'])) {
+        $did = $_REQUEST['did'];
+        $sid = $_REQUEST['sid'];
+        $mrqs = $dbconn->addModRequest($did, $sid);
+    }else {
+        if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_REQUEST['did']) && isset($_REQUEST['sid'])) {
+            $did = $_REQUEST['did'];
+            $sid = $_REQUEST['sid'];
+            $mrqs = $dbconn->addModRequest($did, $sid);
+        }
+    }
+    $result = array("result" => array("requestparams" => $mrqs));
+    die(json_encode($result));
+}

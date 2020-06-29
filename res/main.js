4ed13d04b19ff792a9661a9fe2eb92690d6ca75b
0xFr0zen <0xfr0zen@gmail.com>
2020-06-29 12:01:41 +0200

    Update main.js

--- res/main.js
+++ res/main.js
@@ -11,7 +11,7 @@ $(document).ready((_) => {
     );
     loadCompanies();
     companyIntervall = setInterval(loadCompanies, 10000);
-    stillloggedinintervall = setInterval(checklogin, 2000);
+    stillloggedinintervall = setInterval(checklogin, 10000);
     $('#search').on('click', (_) => {
         $('.my-search-textfield').fadeIn(function () {
             $('.my-search-textfield').focus();

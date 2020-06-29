5dec25a8964cc1b4b88227577399d31aec92683a
0xFr0zen <0xfr0zen@gmail.com>
2020-06-29 11:23:23 +0200

    Update login.js

--- res/login.js
+++ res/login.js
@@ -1,9 +1,10 @@
 let logininterval;
+
 $(document).ready((_) => {
     logininterval = setInterval(checklogin, 2000);
 });
 function checklogin() {
-    $.getJSON('https://api.ev-op.de/mod/login-status', (data) => {
+    $.getJSON('https://api.ev-op.de/mod/login-status/', (data) => {
         if (data.result.loggedin) {
             document.location.href = '/';
         }

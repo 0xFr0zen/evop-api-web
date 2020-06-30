ea2913a4fdcfe393250026e3269dd16195537627
0xFr0zen <0xfr0zen@gmail.com>
2020-06-30 16:40:54 +0200

    Update cc.php

--- cc.php
+++ cc.php
@@ -1,5 +1,5 @@
 <?php
 
-include_once __DIR__.'/../evop-essentials/checkchanges.php';
+include_once __DIR__.'/../essentials/checkchanges.php';
 
 print(json_encode(checkchanges(), JSON_NUMERIC_CHECK));

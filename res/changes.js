dfa2abedbac78d10695beb8f4c37eddfea4f92fa
0xFr0zen <0xfr0zen@gmail.com>
2020-06-30 16:28:07 +0200

    updates

--- res/changes.js
+++ res/changes.js
@@ -2,17 +2,12 @@ let changesIntervall;
 
 $(document).ready(() => {
 	changesIntervall = setInterval(() => {
-		console.log(resourceArray);
-		$.getJSON(
-			`https://api.ev-op.de/check-changes/`,
-			{ resource: resourceArray, page: window.location.pathname },
-			(data) => {
-				if (data.result) {
-					if (currentSiteHash != data.result.hash) {
-						location.reload();
-					}
+		$.getJSON(`https://api.ev-op.de/check-changes/`, (data) => {
+			if (data.result) {
+				if (currentSiteHash != data.result.hash) {
+					location.reload();
 				}
 			}
-		);
+		});
 	}, 10000);
 });

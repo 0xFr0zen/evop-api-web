a80bf6abb2bc1a57f3a95e9e2bce6e6dd968519a
0xFr0zen <0xfr0zen@gmail.com>
2020-07-03 16:59:47 +0200

    updates

--- res/display.js
+++ res/display.js
@@ -3,51 +3,51 @@ window.loadinglocked = false;
 $(document).ready((_) => {
 	loadProducts();
 	companyIntervall = setInterval(loadProducts, 10000);
-	$("#search").on("click", (_) => {
-		$(".my-search-textfield").fadeIn(function () {
-			$(".my-search-textfield").focus();
+	$('#search').on('click', (_) => {
+		$('.my-search-textfield').fadeIn(function () {
+			$('.my-search-textfield').focus();
 			window.loadinglocked = true;
 		});
 	});
-	$("#settings").on("click", (_) => {
-		$("#dialogs").css("display", "flex");
-		$("#dialogs #settingscompany").show();
-		$("#dialogs #settingscompany").css("display", "flex");
+	$('#settings').on('click', (_) => {
+		$('#dialogs').css('display', 'flex');
+		$('#dialogs #settingscompany').show();
+		$('#dialogs #settingscompany').css('display', 'flex');
 
 		$.getJSON(
-			"https://api.ev-op.de/company/" + companyname + "/information",
+			'https://api.ev-op.de/company/' + companyname + '/information',
 			(data) => {
-				$(".my-companyname-tables-textfield").val(data.result.tables);
-				$("#dialogs").fadeIn(100, function () {
+				$('.my-companyname-tables-textfield').val(data.result.tables);
+				$('#dialogs').fadeIn(100, function () {
 					window.loadinglocked = true;
 
-					$(".my-companyname-tables-textfield").on("change", function () {
+					$('.my-companyname-tables-textfield').on('change', function () {
 						let tablenumber = $(this).val();
-						$("#loadingsave").show();
+						$('#loadingsave').show();
 						$.ajax({
 							url:
-								"https://api.ev-op.de/company/" +
+								'https://api.ev-op.de/company/' +
 								companyname +
-								"/tables/" +
+								'/tables/' +
 								tablenumber +
-								"/",
-							type: "PUT",
+								'/',
+							type: 'PUT',
 							success: async function (tabledata) {
 								if (!tabledata.result) {
-									$(".my-card-label-server-issues").removeClass("hidden");
+									$('.my-card-label-server-issues').removeClass('hidden');
 								} else {
 									if (!tabledata.result.updated) {
-										$(".my-card-label-server-issues").removeClass("hidden");
+										$('.my-card-label-server-issues').removeClass('hidden');
 									}
 								}
 								await wait(500);
-								$("#loadingsave").hide();
+								$('#loadingsave').hide();
 							},
-							dataType: "json",
+							dataType: 'json',
 						});
 					});
-					$(".my-close-settings-button").on("click", (_) => {
-						$("#dialogs").fadeOut(100, function () {
+					$('.my-close-settings-button').on('click', (_) => {
+						$('#dialogs').fadeOut(100, function () {
 							$(document.body).click();
 							window.loadinglocked = false;
 						});
@@ -56,56 +56,56 @@ $(document).ready((_) => {
 			}
 		);
 	});
-	$(".my-deactivate-company-button").on("click", (_) => {
+	$('.my-deactivate-company-button').on('click', (_) => {
 		$.ajax({
 			url:
-				"https://api.ev-op.de/company/" +
+				'https://api.ev-op.de/company/' +
 				encodeURI(companyname) +
-				"/deactivate/",
-			type: "PUT",
+				'/deactivate/',
+			type: 'PUT',
 			success: function (deactivatedata) {
 				if (!deactivatedata.result) {
-					$(".my-card-label-server-issues").removeClass("hidden");
+					$('.my-card-label-server-issues').removeClass('hidden');
 				} else {
 					if (!deactivatedata.result.deactivated.status) {
-						$(".my-card-label-server-issues").removeClass("hidden");
+						$('.my-card-label-server-issues').removeClass('hidden');
 					} else {
-						$("#dialogs").fadeOut(100, function () {
+						$('#dialogs').fadeOut(100, function () {
 							$(document.body).click();
 							location.reload();
 						});
 					}
 				}
 			},
-			dataType: "json",
+			dataType: 'json',
 		});
 	});
-	$(".my-activate-company-button").on("click", (_) => {
+	$('.my-activate-company-button').on('click', (_) => {
 		$.ajax({
 			url:
-				"https://api.ev-op.de/company/" + encodeURI(companyname) + "/activate/",
-			type: "PUT",
+				'https://api.ev-op.de/company/' + encodeURI(companyname) + '/activate/',
+			type: 'PUT',
 			success: function (deactivatedata) {
 				if (!deactivatedata.result) {
-					$(".my-card-label-server-issues").removeClass("hidden");
+					$('.my-card-label-server-issues').removeClass('hidden');
 				} else {
 					if (!deactivatedata.result.activated.status) {
-						$(".my-card-label-server-issues").removeClass("hidden");
+						$('.my-card-label-server-issues').removeClass('hidden');
 					} else {
-						$("#dialogs").fadeOut(100, function () {
+						$('#dialogs').fadeOut(100, function () {
 							$(document.body).click();
 							location.reload();
 						});
 					}
 				}
 			},
-			dataType: "json",
+			dataType: 'json',
 		});
 	});
-	$(window).on("keydown", function (e) {
-		if (e.key == "Escape") {
-			$(".my-search-textfield").fadeOut(100);
-			$("#dialogs").fadeOut(100);
+	$(window).on('keydown', function (e) {
+		if (e.key == 'Escape') {
+			$('.my-search-textfield').fadeOut(100);
+			$('#dialogs').fadeOut(100);
 			$(document.body).click();
 			window.loadinglocked = false;
 		}
@@ -113,19 +113,20 @@ $(document).ready((_) => {
 });
 async function loadProducts() {
 	if (!window.loadinglocked) {
-		$("#producticon").text("hourglass_top");
+		$('#producticon').text('hourglass_top');
 		await wait(200);
 		$.getJSON(
-			"https://api.ev-op.de/company/" + encodeURI(companyname) + "/products",
+			'https://api.ev-op.de/company/' + encodeURI(companyname) + '/products',
 			async (data, status) => {
-				$("#content #products #list").empty();
-				$("#producticon").text(
-					$("storeicon").text() === "hourglass_bottom"
-						? "hourglass_top"
-						: "hourglass_bottom"
+				$('#content #products #list').empty();
+				$('#producticon').text(
+					$('storeicon').text() === 'hourglass_bottom'
+						? 'hourglass_top'
+						: 'hourglass_bottom'
 				);
+				console.log(data);
 				await wait(400);
-				$("#producticon").text("store_mall_directory");
+				$('#producticon').text('store_mall_directory');
 			}
 		);
 	}
--- res/utils.js
+++ res/utils.js
@@ -42,6 +42,55 @@ function Company(name, tables, active) {
 	};
 }
 
+function Product(id, name = '', price = 0.0, group = '', subgroup = '') {
+	this.id = id;
+	this.name = name;
+	this.price = price;
+	this.group = group;
+	this.subgroup = subgroup;
+	this.html = () => {
+		let card = document.createElement('div');
+		card.className = 'mdc-card my-card';
+		let cardContentHolder = document.createElement('div');
+		cardContentHolder.className = 'my-card-content';
+
+		let cardLabel = document.createElement('span');
+		cardLabel.className = 'my-card-label';
+		$(cardLabel).text(this.name);
+		let cardLabel2 = document.createElement('span');
+		cardLabel2.className = 'my-card-label';
+		$(cardLabel2).text(this.price);
+
+		let cardInformation = document.createElement('span');
+		cardInformation.className = 'my-card-information';
+		$(cardInformation).text('Group: ' + this.group);
+
+		let cardInformation2 = document.createElement('span');
+		cardInformation2.className = 'my-card-information';
+		$(cardInformation2).text('subgroup: ' + this.subgroup);
+
+		$(cardContentHolder).append(cardLabel);
+		$(cardContentHolder).append(cardLabel2);
+		$(cardContentHolder).append(cardInformation);
+		$(cardContentHolder).append(cardInformation2);
+
+		let action1 = ActionMaker('Show', 'arrow_forward');
+		$(action1).on('click', () => {
+			let openlink = './product/' + this.id;
+			lastopenedWindow = window.open(openlink);
+		});
+		let cardActionHolder = document.createElement('div');
+
+		cardActionHolder.className = 'mdc-card__actions my-card-action-holder';
+
+		$(cardActionHolder).append(action1);
+
+		$(card).append(cardContentHolder);
+		$(card).append(cardActionHolder);
+		return card;
+	};
+}
+
 function ActionMaker(title, text) {
 	let action1 = document.createElement('button');
 	action1.className =

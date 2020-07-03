43bf7856c7c7cafe068f6ec38e6498caf14f81a1
0xFr0zen <0xfr0zen@gmail.com>
2020-07-03 16:02:27 +0200

    updates

--- res/admin.js
+++ res/admin.js
@@ -1,36 +1,36 @@
 let stillloggedinintervall;
 let checkmodrequestsintervall;
 $(document).ready((_) => {
-    checkmodrequests();
-    stillloggedinintervall = setInterval(checklogin, 60000);
-    checkmodrequestsintervall = setInterval(checkmodrequests, 20000);
+	checkmodrequests();
+	stillloggedinintervall = setInterval(checklogin, 60000);
+	checkmodrequestsintervall = setInterval(checkmodrequests, 20000);
 });
 function checklogin() {
-    $.getJSON('https://api.ev-op.de/mod/login-status', (data) => {
-        if (!data.result.status) {
-            // document.location.href = 'http://fr0zenofficial.bplaced.net/test-apps/';
-        }
-    });
+	$.getJSON('https://api.ev-op.de/mod/login-status', (data) => {
+		if (!data.result.status) {
+			document.location.href = 'http://admin.ev-op.de/login';
+		}
+	});
 }
 function checkmodrequests() {
-    $.getJSON('https://api.ev-op.de/mod/requests', (data) => {
-        if (data.result.mod_requests) {
-            let arr = data.result.mod_requests;
-            $('#moderators').empty();
-            arr.forEach((req_mod) => {
-                $.get(
-                    `https://api.ev-op.de/mod/card/${req_mod.deviceid}/${req_mod.ip}/${req_mod.sessionid}/${req_mod.request_time}/${req_mod.code}`,
-                    (card) => {
-                        $('#moderators').append(card);
-                    },
-                    'html'
-                );
-            });
-        }
-    });
+	$.getJSON('https://api.ev-op.de/mod/requests', (data) => {
+		if (data.result.mod_requests) {
+			let arr = data.result.mod_requests;
+			$('#moderators').empty();
+			arr.forEach((req_mod) => {
+				$.get(
+					`https://api.ev-op.de/mod/card/${req_mod.deviceid}/${req_mod.ip}/${req_mod.sessionid}/${req_mod.request_time}/${req_mod.code}`,
+					(card) => {
+						$('#moderators').append(card);
+					},
+					'html'
+				);
+			});
+		}
+	});
 }
 function wait(timeout) {
-    return new Promise((resolve) => {
-        setTimeout(resolve, timeout);
-    });
+	return new Promise((resolve) => {
+		setTimeout(resolve, timeout);
+	});
 }

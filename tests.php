9f288ae4d6e2da1e5359c91cb0ba33431dbe99f9
0xFr0zen <0xfr0zen@gmail.com>
2020-07-14 11:33:36 +0200

    Create tests.php

---
+++ tests.php
@@ -0,0 +1,45 @@
+<?php
+
+class Tester {
+
+    private string $BASE_URL = "https://api.ev-op.de";
+    private array $testlinks;
+
+    public function __construct(){
+        $this->testlinks = json_decode(file_get_contents(__DIR__.PATH_SEPARATOR.'tests'.PATH_SEPARATOR.'links.json'), true);
+    }
+
+    private function post_request($url, array $params = array()) {
+        $query_content = http_build_query($params);
+        $fp = fopen($url, 'r', FALSE, // do not use_include_path
+            stream_context_create([
+            'http' => [
+            'header'  => [ // header array does not need '\r\n'
+                'Content-type: application/x-www-form-urlencoded',
+                'Content-Length: ' . strlen($query_content)
+            ],
+            'method'  => 'POST',
+            'content' => $query_content
+            ]
+        ]));
+
+        if ($fp === FALSE) {
+            return json_encode(['error' => 'Failed to get contents...']);
+        }
+
+        $result = stream_get_contents($fp); // no maxlength/offset
+        fclose($fp);
+        return $result;
+    }
+
+    public function run(){
+        foreach ($testlinks as $key => $value) {
+            print($value);
+        }
+    }
+
+}
+
+
+$tester = new Tester();
+$tester->run();

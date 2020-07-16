1dd8fc104a62693b0c3fb07cb0416f99a3ba85de
0xFr0zen <0xfr0zen@gmail.com>
2020-07-16 11:48:08 +0200

    Update get.php

--- user-methods/types/get.php
+++ user-methods/types/get.php
@@ -2,14 +2,19 @@
 include_once __DIR__.'/../requirements.php';
 class ReqMethod extends ReqUser implements ReqInterface {
     public function execute(){
-        switch ($this->mode) {
-            case 'status':
-                $this->result = array("status" => $this->user->status($this->details));
-                break;
-            default:
-            $this->result = array("status" => $this->user->status('online'));
-                break;
+        if(!$this->exists){
+            $this->result = array("error" => array("message" => User::$USER_NONEXISTING));
+        }else {
+            switch ($this->mode) {
+                case 'status':
+                    $this->result = array("status" => $this->user->status($this->details));
+                    break;
+                default:
+                $this->result = array("status" => $this->user->status('online'));
+                    break;
+            }
         }
+        
         
     }
 }
